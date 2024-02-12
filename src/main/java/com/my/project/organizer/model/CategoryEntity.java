c6ad885dd34c87f5776b00526325fce32306b819
João Pacheco <jvep990@gmail.com>
2024-02-12 04:49:02 -0300

    h2 database configuration

--- src/main/java/com/my/project/organizer/model/CategoryEntity.java
+++ src/main/java/com/my/project/organizer/model/CategoryEntity.java
@@ -1,12 +1,16 @@
 package com.my.project.organizer.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryEntity {
 
     @Id
--- src/main/java/com/my/project/organizer/model/TaskEntity.java
+++ src/main/java/com/my/project/organizer/model/TaskEntity.java
@@ -1,12 +1,16 @@
 package com.my.project.organizer.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskEntity {
 
     @Id
